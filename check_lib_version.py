82cb9eed01c937c244717377165e810e0882e993
Rathanak Sreang <sreang.rathanak@framgia.com>
2017-05-29 17:14:54 +0700

    First project

---
+++ check_lib_version.py
@@ -0,0 +1,24 @@
+# check Python version and some lib version
+
+import sys
+print('Python: {}'.format(sys.version))
+
+#scipy
+import scipy
+print('Scipy: {}'.format(scipy.__version__))
+
+#numpy
+import numpy
+print('Numpy: {}'.format(numpy.__version__))
+
+# matplotlib
+import matplotlib
+print('Matplotlib: {}'.format(matplotlib.__version__))
+
+# pandas
+import pandas
+print('Pandas: {}'.format(pandas.__version__))
+
+# scikit-learn
+import sklearn
+print('SKlearn: {}'.format(sklearn.__version__))

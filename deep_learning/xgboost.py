e416e7b94be291a3773df8d93a815833585efdfe
Rathanak Sreang <sreang.rathanak@framgia.com>
2017-06-09 16:32:02 +0700

    Keras nn lession

---
+++ deep_learning/xgboost.py
@@ -0,0 +1,19 @@
+from numpy import loadtxt
+from xgboost import XGBClassifier
+from xgboost import plot_importance
+from matplotlib import pyplot
+
+#load data
+dataset = loadtxt('pima-indians-diabetes.csv', delimiter=",")
+
+#split data into X, y
+X = dataset[:, 0:8]
+y = dataset[:, 8]
+
+#fit model no training data
+model = XGBClassifier()
+model.fit(X,y)
+
+#plot feature importance
+plot_importance(model)
+pyplot.show()

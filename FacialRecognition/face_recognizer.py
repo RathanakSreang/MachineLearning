b2919b12f2aab5cfaee90b63a57e604848c8552c
Sreang Rathanak <sreangrathanak@yahoo.com>
2017-10-23 23:30:19 +0700

    Learning

---
+++ FacialRecognition/face_recognizer.py
@@ -0,0 +1,18 @@
+# read folder name in images folder
+# names = get folder name
+# conver names to np array [1,2,3,4,5]
+
+# read images in folders
+# - loop in folder name
+# -    get image
+# convert image to np
+# - facearr = np.array(face_image).astype(np.float32)
+
+# init model
+# add layer
+# train model
+# test model
+# predict
+# save model
+
+# predict
